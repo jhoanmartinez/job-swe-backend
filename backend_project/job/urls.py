caf8ed42452a47136556425e2ff412f04d87e1f5
jhoanmartinez <jhoao1320@gmail.com>
2023-09-29 19:16:37 -0500

    crup operations per job

--- backend_project/job/urls.py
+++ backend_project/job/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import GetAllJobs
+from .views import GetAllJobs, GetJobByID, PostJob, UpdateJob, DeleteJob
 
 urlpatterns = [
-    path('jobs/', GetAllJobs.as_view(), name="jobs")
+    path('all/', GetAllJobs.as_view(), name="jobs"),
+    path('<int:job_id>/', GetJobByID.as_view(), name='job-detail'),
+    path('new/', PostJob.as_view(), name='new-job'),
+    path('update/<job_id>/', UpdateJob.as_view(), name='update-job'),
+    path('delete/<job_id>/', DeleteJob.as_view(), name='delete-job')
 ]
--- backend_project/job/views.py
+++ backend_project/job/views.py
@@ -1,11 +1,55 @@
-from rest_framework import views
+from rest_framework.views import APIView
 from .models import Job
 from .serializers import JobSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
-class GetAllJobs(views.APIView):
+class GetAllJobs(APIView):
 
     def get(self, request):
         jobs = Job.objects.all()
         serializer = JobSerializer(jobs, many=True)
         return Response(serializer.data)
+
+class GetJobByID(APIView):
+
+    def get(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+            serializer = JobSerializer(job)
+            return Response(serializer.data)
+        except Job.DoesNotExist:
+            return Response({"message": "El trabajo no existe"}, status=status.HTTP_404_NOT_FOUND)
+
+class PostJob(APIView):
+
+    def post(self, request):
+        print(request.data)
+        serializer = JobSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UpdateJob(APIView):
+
+    def patch(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+        except Job.DoesNotExist:
+            return Response( {'message': 'Job not exist'}, status=status.HTTP_404_NOT_FOUND )
+        serializer = JobSerializer(job, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteJob(APIView):
+
+    def delete(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+        except Job.DoesNotExist:
+            return Response( {'message': 'Job not exist'}, status=status.HTTP_404_NOT_FOUND )
+        job.delete()
+        return Response( {'message': 'Job deleted succesfully'}, status=status.HTTP_204_NO_CONTENT )
