9dd9891cdac721b5e57a8d26808b0f1564fc4d58
jhoanmartinez <jhoao1320@gmail.com>
2023-10-01 15:09:20 -0500

    filtlers

--- backend_project/job/filters.py
+++ backend_project/job/filters.py
@@ -3,6 +3,9 @@ from .models import Job
 
 class JobFilter(filters.FilterSet):
 
+    min_salary = filters.NumberFilter(field_name='salary' or 0, lookup_expr='gte')
+    max_salary = filters.NumberFilter(field_name='salary' or 10000000, lookup_expr='lte')
+
     class Meta:
         model = Job
         fields = ('education', 'jobType', 'experience', 'title')
